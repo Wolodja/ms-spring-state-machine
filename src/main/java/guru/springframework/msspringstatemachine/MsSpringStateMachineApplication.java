6cb60ac911ed820b79b6306e9377c67c830487ed
Wolodja <wolodjawaskovych@i.ua>
2021-02-06 11:53:35 +0100

    Initial version

---
+++ src/main/java/guru/springframework/msspringstatemachine/MsSpringStateMachineApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msspringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSpringStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/msspringstatemachine/MsSpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msspringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
